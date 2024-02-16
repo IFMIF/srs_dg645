5837270a06b9bb10593b0bd5dec7f5f3c02f4d6c
Florian Benedetti <florian.benedetti@cea.fr>
2024-02-16 08:18:04 +0100

    General upgrade of the DG645 support.
    - Add channel and output for better control and visualization
    - Correct some bug
    - Update the DB and protofile
    - Update the OPI
    
    Still work in progress but at least the user can see its modification.

--- iocBoot/iocDG645/st.cmd
+++ iocBoot/iocDG645/st.cmd
@@ -29,6 +29,22 @@ epicsThreadSleep(2)
 ## Load record instances
 #dbLoadRecords("db/asynRecord.db","P=$(P),R=$(R),PORT=L0,ADDR=0,IMAX=200,OMAX=200")
 dbLoadRecords("db/DG645.db","P=$(P),R=$(R),PORT=L0,A=0")
+dbLoadRecords("db/Output.db","P=$(P),R=$(R),N=T0:,ID=0,PORT=L0,A=0")
+dbLoadRecords("db/Output.db","P=$(P),R=$(R),N=AB:,ID=1,PORT=L0,A=0")
+dbLoadRecords("db/Output.db","P=$(P),R=$(R),N=CD:,ID=2,PORT=L0,A=0")
+dbLoadRecords("db/Output.db","P=$(P),R=$(R),N=EF:,ID=3,PORT=L0,A=0")
+dbLoadRecords("db/Output.db","P=$(P),R=$(R),N=GH:,ID=3,PORT=L0,A=0")
+
+dbLoadRecords("db/Channel.db","P=$(P),R=$(R),N=t0:,ID=0,PORT=L0,A=0")
+dbLoadRecords("db/Channel.db","P=$(P),R=$(R),N=t1:,ID=1,PORT=L0,A=0")
+dbLoadRecords("db/Channel.db","P=$(P),R=$(R),N=a:,ID=2,PORT=L0,A=0")
+dbLoadRecords("db/Channel.db","P=$(P),R=$(R),N=b:,ID=3,PORT=L0,A=0")
+dbLoadRecords("db/Channel.db","P=$(P),R=$(R),N=c:,ID=4,PORT=L0,A=0")
+dbLoadRecords("db/Channel.db","P=$(P),R=$(R),N=d:,ID=5,PORT=L0,A=0")
+dbLoadRecords("db/Channel.db","P=$(P),R=$(R),N=e:,ID=6,PORT=L0,A=0")
+dbLoadRecords("db/Channel.db","P=$(P),R=$(R),N=f:,ID=7,PORT=L0,A=0")
+dbLoadRecords("db/Channel.db","P=$(P),R=$(R),N=g:,ID=8,PORT=L0,A=0")
+dbLoadRecords("db/Channel.db","P=$(P),R=$(R),N=h:,ID=9,PORT=L0,A=0")
 
 
 cd "${TOP}/iocBoot/${IOC}"
